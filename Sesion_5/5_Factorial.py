66359056095fc04958cf6be053a073a3cb4ec541
JoseKoalas <josepazosperez@mundo-r.com>
2019-10-21 18:17:44 +0200

    Create 5_Factorial.py

---
+++ Sesion_5/5_Factorial.py
@@ -0,0 +1,11 @@
+#Factorial
+import math
+
+#Con factorial de Python
+n = int(input("n = "))
+for i in range(n+1):
+    print("Factorial de {}= {}".format(i, math.factorial(i)))
+
+#Creación Propia
+def calc_factorial():
+    
